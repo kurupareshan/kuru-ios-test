0805c66f55eca681be35d2741dbb0853135467e0
kuru <p.kuru2014@gmail.com>
2024-04-30 08:05:13 +0530

    Initial Commit

---
+++ KuruTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KuruTest
+//
+//  Created by Kuru on 2024-04-30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
